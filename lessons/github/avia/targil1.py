967af0cc2089832cdf24554144d8ab1ed2743307
Gal Weiss <weiss_gal@yahoo.com>
2023-03-16 11:02:29 +0200

    added targil for everyone else

---
+++ lessons/github/avia/targil1.py
@@ -0,0 +1,28 @@
+# you mission, should you choose to accept it, is to fix the following function.
+# here are the steps:
+# 1. read the function description 
+# 2. run the code 
+# 3. find the bug 
+# 4. fix the bug
+# 5. push the fix back to github
+
+
+# this function will print all the numbers from 'start' to 'stop'
+# example:
+# print the first number: -1
+# print the last number: 3
+# -1
+# 0
+# 1
+# 2
+# 3
+
+def print_nums(start, stop):
+    for i in range(start, stop, 1):
+        print(i)
+
+start = int(input("print the first number: "))
+stop = int(input("print the last number: "))
+
+print_nums(start, stop)
+
