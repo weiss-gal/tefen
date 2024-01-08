74efa02c251c886a28d425d64c393d1deb0d6f1a
Gal Weiss <weiss-gal@users.noreply.github.com>
2024-01-08 06:02:56 +0200

    Create test_exp.py

---
+++ 2023_2024/8th_grade/lessons/00_vars_and_expresssions/test_exp.py
@@ -0,0 +1,47 @@
+# write down on a piece of paper the expected output
+print("The answer to question number 1 is:")
+print((1+2)*3+4*5)
+print()
+
+print("The answer to question number 2 is:")
+print(type(1+1))
+print()
+
+print("The answer to question number 3 is:")
+print("@#$%^&*()")
+print()
+
+print("The answer to question number 4 is:")
+print(type("@" + "#" + "$%^&*" + "()"))
+print()
+
+print("The answer to question number 5 is:")
+print("type(1)")
+print()
+
+print("The answer to question number 6 is:")
+print("1"*10)
+print()
+
+print("The answer to question number 7 is:")
+print("ba" + ("na"*2) + " s" + "hake")
+print()
+
+print("The answer to question number 8 is:")
+print((100+100)/100)
+print()
+
+print("*** bonus question ***")
+print("The answer to question number 9 is:")
+print(22/7)
+print()
+
+print("*** bonus question ***")
+print("The answer to question number 10 is:")
+print(type(1/2))
+print()
+
+
+
+
+
