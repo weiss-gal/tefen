17bc147a9f0b670ece8cd4221af3feb1148ab78c
Gal Weiss <weiss_gal@yahoo.com>
2024-09-30 18:11:21 +0300

    added lesson

---
+++ 2024_2025/10th_grade/lessons/00_values_and_variables/solution.py
@@ -0,0 +1,14 @@
+# read number from the user
+user_input = input("please enter a number: ")
+
+# convert the user input to a number
+x = int(user_input)
+
+# check if the number is greater or lower than 5
+
+if x > 5:
+    print("The number is greater than 5")
+
+if x < 5:
+    print("The number is lower than 5")
+
