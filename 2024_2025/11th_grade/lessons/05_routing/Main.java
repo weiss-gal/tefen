1cf962add2104f76ef52b309b3aa552fb7c9eba0
Gal Weiss <weiss_gal@yahoo.com>
2024-12-23 01:37:14 +0200

    router - work in progress

--- 2024_2025/11th_grade/lessons/05_routing/Main.java
+++ 2024_2025/11th_grade/lessons/05_routing/Main.java
@@ -1,16 +1,7 @@
-class Router {
-    // for example 101 -> "A", 102 -> "Tel Aviv"
-    void addRoute(int address, String target) {
-
-    }
-
-    String resolveRoute(int address) {
-        return "Tel Aviv ";
-    }
-}
 
 
 public class Main {
+
     public static void main(String[] args) {
         var router = new Router();
 
